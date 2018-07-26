5b497467ef08686da0486a34a761e4bf5bee4fd1
KostNazvanov <nazvanov.Kst@gmail.com>
2018-07-26 10:11:50 +0300

    Update example to use new Layout.StructureItem

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,39 @@ import { AuthContentProps, AuthFormProps, DummyAuth } from 'webpanel-auth';
 import { users } from './pages/users';
 
 const layout = (props: AuthContentProps) => (
-  <Layout
-    menu={[
-      <Layout.MenuItem
-        key="/"
-        path="/"
-        title="Dashboard"
-        icon="exclamation-circle-o"
-      />,
-      <Layout.MenuItem path="/users/" key="/users/" title="Users" icon="user" />
-    ]}
-    structure={{
-      '/': {
-        content: 'Hello world, welcome in demo app.',
-        name: 'Dashboard'
-      },
-      '/users': { name: 'Users', content: users },
-      '/users/:id': {
-        content: (route: RouteComponentProps<any>) => {
-          return `This is detail page for user ${route.match.params.id}`;
-        },
-        name: 'Users'
-      }
-    }}
-    logout={props.logout}
-  />
+    <Layout
+        menu={[
+            <Layout.MenuItem
+                key="/"
+                path="/"
+                title="Dashboard"
+                icon="exclamation-circle-o"
+            />,
+            <Layout.MenuItem
+                path="/users/"
+                key="/users/"
+                title="Users"
+                icon="user"
+            />
+        ]}
+        structure={{
+            '/': <Layout.StructureItem
+                content="Hello world, welcome in demo app."
+                name="Dashboard"
+            />,
+            '/users': <Layout.StructureItem
+                name="Users"
+                content={users}
+            />,
+            '/users/:id': <Layout.StructureItem
+                content={(route: RouteComponentProps<any>) => {
+                    return `This is detail page for user ${route.match.params.id}`;
+                }}
+                name="Users"
+            />
+        }}
+        logout={props.logout}
+    />
 );
 
 export const content = (
