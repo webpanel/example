2ba92d847ec7540e3f43bb8ed3a94092cbd9b7d5
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-08-08 01:46:28 +0200

    Update layout and webpanel-antd to 0.0.16

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,14 @@ const layout = (props: AuthContentProps) => (
   <Layout logout={props.logout}>
     <Layout.Menu>
       <Layout.MenuItem key="/" title="Dashboard" icon="exclamation-circle-o" />
-      <Layout.MenuItem key="/users/" title="Users" icon="user" />
-      <Layout.MenuItem key="/charts/" title="Charts" icon="line-chart" />
+      <Layout.MenuItem key="users" title="Users" icon="user" />
+      <Layout.MenuItem key="charts" title="Charts" icon="line-chart" />
+      <Layout.MenuItem key="menu" title="Menu tree" icon="folder">
+        <Layout.MenuItem key="item1" title="Item 1" icon="folder" />
+        <Layout.MenuItem key="item2" title="Item 2" icon="folder">
+          <Layout.MenuItem key="notfound" title="Not found" icon="folder" />
+        </Layout.MenuItem>
+      </Layout.MenuItem>
     </Layout.Menu>
     <Layout.Structure>
       <Layout.StructureItem
@@ -19,15 +25,23 @@ const layout = (props: AuthContentProps) => (
         content="Hello world, welcome in demo app."
         name="Dashboard"
       />
-      <Layout.StructureItem key="/users" name="Users" content={users} />,
-      <Layout.StructureItem
-        key="/users/:id"
-        content={(route: RouteComponentProps<any>) => {
-          return userDetail(route.match.params.id);
-        }}
-        name="Users"
-      />
-      <Layout.StructureItem key="/charts" name="Charts" content={charts} />
+      <Layout.StructureItem key="users" name="Users" content={users}>
+        <Layout.StructureItem
+          key=":id"
+          content={(route: RouteComponentProps<any>) => {
+            return userDetail(route.match.params.id);
+          }}
+          name="Detail"
+        />
+      </Layout.StructureItem>
+      <Layout.StructureItem key="charts" name="Charts" content={charts} />
+      <Layout.StructureItem key="menu" name="Tree">
+        <Layout.StructureItem
+          key="item1"
+          name="Item 1"
+          content={'Menu tree page1 content'}
+        />
+      </Layout.StructureItem>
     </Layout.Structure>
   </Layout>
 );
