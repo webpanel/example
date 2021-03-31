72038c407c04d525109afb8d07873bc646bf601b
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2021-03-31 16:44:03 +0200

    Remove unnecessary fields

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,6 @@ export const App = () => {
       logout={() => {
         // do logout
       }}
-      // auth={{
-      //   type: "oauth",
-      //   oauthTokenURL: ENV.REACT_APP_TOKEN_URL,
-      //   clientId: ENV.REACT_APP_CLIENT_ID,
-      //   clientSecret: ENV.REACT_APP_CLIENT_SECRET,
-      //   scope: ENV.REACT_APP_SCOPE,
-      //   userNameGetter: (session: AuthSession) => {
-      //     const payload = session.getTokenPayload();
-      //     return payload && payload.user && payload.user.username;
-      //   },
-      // }}
       logoURL="/logo-wide.png"
       logoCollapsedURL="/logo.png"
       menu={{ theme: "dark" }}
